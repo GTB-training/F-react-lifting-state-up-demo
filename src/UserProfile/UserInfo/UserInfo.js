ce1618014aac30da76bcd8131df3b69c2607e4bd
杜娟 <jdu.xian@gmail.com>
2020-05-22 15:39:20 +0800

    add user info

--- src/UserProfile/UserInfo/UserInfo.js
+++ src/UserProfile/UserInfo/UserInfo.js
@@ -2,8 +2,46 @@ import React, { Component } from 'react';
 import './UserInfo.scss';
 
 class UserInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {},
+    };
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        user: {
+          name: 'Nana',
+          statistics: {
+            posts: 1000,
+            followers: 100,
+            following: 200,
+          },
+        },
+      });
+    }, 1000);
+  }
+
   render() {
-    return <section className="UserInfo">UserInfo</section>;
+    const { name, statistics } = this.state.user;
+    return (
+      <section className="UserInfo">
+        <div className="avatar">
+          <div className="fake-image" />
+        </div>
+
+        <section className="user">
+          <h2>{name}</h2>
+          <ul>
+            <li>{statistics ? statistics.posts : null} posts</li>
+            <li>{statistics ? statistics.followers : null} followers</li>
+            <li>{statistics ? statistics.following : null} following</li>
+          </ul>
+        </section>
+      </section>
+    );
   }
 }
 
